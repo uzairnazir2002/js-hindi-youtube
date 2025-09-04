a3b59e29aeeb0be33ca2b4eaedf2e34589a21c99
uzairnazir2002 <Uzairnazir42@gmail.com>
2025-09-04 20:08:12 +0000

    Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = 98;
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+//"" => false , "uzair" => true
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = "33";
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
