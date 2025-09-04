e4116a3c968422adab105167ec9ea5f074f93d87
uzairnazir2002 <Uzairnazir42@gmail.com>
2025-09-04 19:11:57 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+
+//alert (3+3) //We are using node js, not browser
+
+console.log(3+3); //Code readablity should be high
+console.log("Uzair") 
+
+let name = "Fahad";
+let age = 18;
+let isLoggedIn = false;
+let state ;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => staandalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+console.log(typeof undefined);
